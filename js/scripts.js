77076dc634e68be37f534f840e9905f893c3b5c4
Tolga Cebeci <tolgacebeci1994@gmail.com>
2026-03-25 21:47:00 +0300

    Updated carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,6 @@
 const track = document.querySelector('.carousel-track');
-let position = 0;
-let speed = 0.3;
-let targetSpeed = 0.3;
-let lastTime = null;
-
-function animate(time) {
-  if (lastTime !== null) {
-    const delta = time - lastTime;
-    position -= speed * delta;
-    track.style.transform = `translateX(${position}px)`;
-
-    speed += (targetSpeed - speed) * 0.1;
-
-    const width = track.scrollWidth / 2;
-    if (Math.abs(position) > width) {
-      position = 0;
-    }
-  }
-  lastTime = time;
-  requestAnimationFrame(animate);
-}
-
-requestAnimationFrame(animate);
-
-track.parentElement.addEventListener('mouseenter', () => {
-  targetSpeed = 0;
-});
-
-track.parentElement.addEventListener('mouseleave', () => {
-  targetSpeed = 0.3;
-});
+const all = [...games, ...games];
+track.innerHTML = all.map(game =>
+  `<a href="${game.url}" target="_blank"><img src="${game.image}" alt="${game.name}"></a>`
+).join('');
+track.style.animationDuration = (games.length * 3) + 's';
